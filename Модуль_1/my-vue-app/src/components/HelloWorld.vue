3eb26c737338fa6eb7d909cc5aa0f40dda2f483e
Vanchouses <ivan_shevelev2007@mail.ru>
2025-06-13 16:44:46 +0300

    Create HelloWorld.vue

---
+++ Модуль_1/my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <button @click = "changeMassage">Сменить сообщение</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('Привет Vue 3 с Compasition API')
+
+function changeMassage() {
+    message.value = "NewMassage";
+}
+</script>
+
+<style scoped>
+h1 {
+    color: aqua;
+}
+</style>
