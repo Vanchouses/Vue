0dbe6b9fa793a6273bece1414f933050237efa8b
Vanchouses <ivan_shevelev2007@mail.ru>
2025-07-02 09:41:33 +0300

    Avion-add-features2

--- Project-Avion/src/App.vue
+++ Project-Avion/src/App.vue
@@ -2,20 +2,22 @@
   <AvionHead />
   <AvionNav />
   <AvionChear />
-  <AvionFuters />
+  <AvionFeatures />
   <AvionListings1 />
   <AvionListings2 />
   <AvionEmailSection />
+  <AvionFeatures2 />
 </template>
 
 <script setup>
   import AvionHead from './components/Avion-head.vue';
   import AvionNav from './components/Avion-nav.vue';
   import AvionChear from './components/Avion-chear.vue';
-  import AvionFuters from './components/Avion-futers.vue';
+  import AvionFeatures from './components/Avion-features1.vue';
   import AvionListings1 from './components/Avion-listings1.vue';
   import AvionListings2 from './components/Avion-listings2.vue';
   import AvionEmailSection from './components/Avion-Email-section.vue';
+  import AvionFeatures2 from './components/Avion-features2.vue';
 </script>
 
 <style scoped>
---
+++ Project-Avion/src/components/Avion-features2.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="main-div">
+    <div>
+        <h1>From a studio in London to a global brand with
+over 400 outlets</h1>
+        <p>When we started Avion, the idea was simple. Make high quality 
+        furniture affordable and available for the mass market.Handmade, 
+        and lovingly crafted furniture and homeware is what we live, breathe 
+        and design so our Chelsea boutique become the hotbed for the London 
+        interior design community.</p>
+
+        <button class="get-in-touch">Get in touch</button>
+    </div>
+    <div>
+        <img :src="SofaPng" alt="">
+    </div>
+</div>
+</template>
+
+<script setup>
+import SofaPng from '../assets/sofa.png';
+</script>
+
+<style scoped>
+</style>
